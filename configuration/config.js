240c7b275c13ccc024a3b2fe055b8eecb91539ae
unknown <parasharamantyagi@gmail.com>
2020-04-26 12:40:56 +0530

    my node with twitter project

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"twitter_api_key" 		: 			"1a8NCZSUiYnyp9RqYKBgp1Xm6",
+	"twitter_api_secret"	:			"zJ3UfdO0UdXxK2s0k6yz26XsgQPbEu11mFciDONXPznP8ktyeG",
+	"callback_url"			:			"http://mytechnologyes.com:3000/twitter/callback",
+	"use_database"			:			"true",
+	"host"					:			"localhost",
+	"username"				:			"root",
+	"password"				:			"",
+	"database"				:			"ci_api"
+}
